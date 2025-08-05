ab97192f1ab8e6c23696fa671dfa14182b0285f9
ManojChopkarEspire <manoj.chopkar@espire.com>
2025-08-05 10:55:54 +0530

    code  cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { onEntryChange } from '../contentstack-sdk';
-import Skeleton from 'react-loading-skeleton';
-import FormDocument from '../components/form-document';
+import FormDocument from "../components/GenerateContent";
+// import GenerateContent from '../components/GenerateContent/GenerateContent';
 
-import { Props, Context } from "../typescript/pages";
-
-
-export default function Home(props: Props) {
-  const { page, entryUrl } = props;
-  const [getEntry, setEntry] = useState(page);
-  
-  // async function fetchData() {
-  //   try {
-  //     const entryRes = await getPageRes(entryUrl);
-  //     if (!entryRes) throw new Error('Status code 404');
-  //     setEntry(entryRes);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   onEntryChange(() => fetchData());
-  // }, []);
-
+export default function Home() {
   return (
     <>
+      {/* <GenerateContent /> */}
       <FormDocument />
     </>
   );
-}
-
-export async function getServerSideProps(context: Context) {
-  try {
-    // const entryRes = await getPageRes(context.resolvedUrl);
-    // return {
-    //   props: {
-    //     entryUrl: context.resolvedUrl,
-    //     page: entryRes,
-    //   },
-    // };
-    return {
-      props: {
-        entryUrl: context.resolvedUrl,
-        page: null,
-      },
-    };
-  } catch (error) {
-    return { notFound: true };
-  }
 }
