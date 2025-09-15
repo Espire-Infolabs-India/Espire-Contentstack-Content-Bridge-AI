9e56e33464ce681eb0e2b58e1714c14404859521
Atul_Gusain <atul.gusain@espire.com>
2025-09-15 09:49:39 +0530

    code changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getStackInfo, SafeStackInfo } from "../helper/get-stack-details";
 import GenerateContent from "../components/GenerateContent";
 
 export default function Home() {
+  const [stackData, setStackData] = useState<SafeStackInfo | null>(null);
+
+  useEffect(() => {
+    const run = async () => {
+      const data = await getStackInfo();
+      if (data) {
+        console.log("stackdata", data);
+        setStackData(data);
+      }
+    };
+    run();
+  }, []);
   return (
     <>
+    {stackData && (
+        <>
+          <h3>Stack Name: {stackData.stackname}</h3>
+          <h3>API Key: {stackData.apiKey}</h3>
+          <h3>Branch: {stackData.branch}</h3>
+          <h3>CMA TOKEN: {stackData.cmaToken}</h3>
+          <h3>Delivery Token: {stackData.deliveryToken}</h3>
+          <h3>Org UID: {stackData.org_uid}</h3>
+          <h3>Owner UID: {stackData.owner_uid}</h3>
+          <h3>Region: {stackData.appRegion}</h3>
+        </>
+      )}
       <GenerateContent />
     </>
   );
